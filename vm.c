d959baf3bdc11c328711e36ab58da6f012b87dac
Andrew Yang <andrewhuiyang0830@gmail.com>
2021-09-11 14:18:52 -0700

    hook super up to run()

--- vm.c
+++ vm.c
@@ -374,6 +374,15 @@ static InterpretResult run() {
                 push(value);
                 break;
             }
+            case OP_GET_SUPER: {
+                ObjString* name = READ_STRING();
+                ObjClass* superclass = AS_CLASS(pop());
+
+                if (!bindMethod(superclass, name)) {
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                break;
+            }
             case OP_EQUAL: {
                 Value b = pop();
                 Value a = pop();
