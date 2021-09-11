833cae7e57662ed93beb66bd3fec064be83a2d2c
Andrew Yang <andrewhuiyang0830@gmail.com>
2021-09-11 14:07:19 -0700

    super keyword

--- compiler.c
+++ compiler.c
@@ -507,6 +507,13 @@ static void variable(bool canAssign) {
     namedVariable(parser.previous, canAssign);
 }
 
+static Token syntheticToken(const char* text) {
+    Token token;
+    token.start = text;
+    token.length = (int)strlen(text);
+    return token;
+}
+
 static void this_(bool canAssign) {
     if (currentClass == NULL) {
         error("Can't use 'this' outside of a class.");
@@ -677,6 +684,10 @@ static void classDeclaration() {
         error("A class can't inherit from itself.");
     }
 
+    beginScope();
+    addLocal(syntheticToken("super"));
+    defineVariable(0);
+
     namedVariable(className, false);
     consume(TOKEN_LEFT_BRACE, "Expect '{' before class body.");
     while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
@@ -684,6 +695,10 @@ static void classDeclaration() {
     }
     consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body.");
     emitByte(OP_POP);
+
+//    if (classCompiler.hasSuperclass) {
+//        endScope();
+//    }
 
     currentClass = currentClass->enclosing;
 }
--- vm.c
+++ vm.c
@@ -480,6 +480,9 @@ static InterpretResult run() {
                 break;
             case OP_INHERIT: {
                 Value superclass = peek(1);
+                if (!IS_CLASS(superclass)) {
+                    runtimeError("Superclass must be a class.");
+                    return INTERPRET_RUNTIME_ERROR;
                 ObjClass* subclass = AS_CLASS(peek(0));
                 tableAddAll(&AS_CLASS(superclass)->methods, &subclass->methods);
                 pop(); // Subclass.
