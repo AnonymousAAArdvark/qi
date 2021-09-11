0fdad9b326d90e5c8743d583a7eea4aaacad4666
Andrew Yang <andrewhuiyang0830@gmail.com>
2021-09-11 13:59:16 -0700

    OP_INHERIT implementation

--- debug.c
+++ debug.c
@@ -141,6 +141,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return simpleInstruction("OP_RETURN", offset);
         case OP_CLASS:
             return constantInstruction("OP_CLASS", chunk, offset);
+        case OP_INHERIT:
+            return simpleInstruction("OP_INHERIT", offset);
         case OP_METHOD:
             return constantInstruction("OP_METHOD", chunk, offset);
         default:
--- vm.c
+++ vm.c
@@ -478,6 +478,13 @@ static InterpretResult run() {
             case OP_CLASS:
                 push(OBJ_VAL(newClass(READ_STRING())));
                 break;
+            case OP_INHERIT: {
+                Value superclass = peek(1);
+                ObjClass* subclass = AS_CLASS(peek(0));
+                tableAddAll(&AS_CLASS(superclass)->methods, &subclass->methods);
+                pop(); // Subclass.
+                break;
+            }
             case OP_METHOD:
                 defineMethod(READ_STRING());
                 break;
