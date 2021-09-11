1aed9a12d58972a6f5520ea78c1c97a3bcb5656b
Andrew Yang <andrewhuiyang0830@gmail.com>
2021-09-11 14:25:30 -0700

    optimization in super_

--- compiler.c
+++ compiler.c
@@ -527,8 +527,15 @@ static void super_(bool canAssign) {
     uint8_t name = identifierConstant(&parser.previous);
 
     namedVariable(syntheticToken("this"), false);
-    namedVariable(syntheticToken("super"), false);
-    emitBytes(OP_GET_SUPER, name);
+    if (match(TOKEN_LEFT_PAREN)) {
+        uint8_t argCount = argumentList();
+        namedVariable(syntheticToken("super"), false);
+        emitBytes(OP_SUPER_INVOKE, name);
+        emitByte(argCount);
+    } else {
+        namedVariable(syntheticToken("super"), false);
+        emitBytes(OP_GET_SUPER, name);
+    }
 }
 
 static void this_(bool canAssign) {
