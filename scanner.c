001ff4ec74ac9721fca2a3bc9dc4c0a1a1165432
Andrew Yang <andrewhuiyang0830@gmail.com>
2021-09-13 23:42:36 -0700

    added switch to scanners

--- scanner.c
+++ scanner.c
@@ -114,7 +114,14 @@ static TokenType checkKeyword(int start, int length, const char* rest, TokenType
 static TokenType identifierType() {
     switch (scanner.start[0]) {
         case 'a': return checkKeyword(1, 2, "nd", TOKEN_AND);
-        case 'c': return checkKeyword(1, 4, "lass", TOKEN_CLASS);
+        case 'c':
+            if (scanner.current - scanner.start > 1) {
+                switch (scanner.start[1]) {
+                    case 'l': return checkKeyword(2, 4, "ass", TOKEN_CLASS);
+                    case 'a': return checkKeyword(2, 2, "se", TOKEN_CASE);
+                }
+            }
+        case 'd': return checkKeyword(1, 6, "efault", TOKEN_DEFAULT);
         case 'e': return checkKeyword(1, 3, "lse", TOKEN_ELSE);
         case 'f':
             if (scanner.current - scanner.start > 1) {
@@ -130,7 +137,13 @@ static TokenType identifierType() {
         case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
         case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT);
         case 'r': return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
-        case 's': return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+        case 's':
+            if (scanner.current - scanner.start > 1) {
+                switch (scanner.start[1]) {
+                    case 'u': return checkKeyword(2, 4, "per", TOKEN_SUPER);
+                    case 'w': return checkKeyword(2, 4, "itch", TOKEN_SWITCH);
+                }
+            }
         case 't':
             if (scanner.current - scanner.start > 1) {
                 switch (scanner.start[1]) {
@@ -200,6 +213,7 @@ Token scanToken() {
         case '+': return makeToken(TOKEN_PLUS);
         case '/': return makeToken(TOKEN_SLASH);
         case '*': return makeToken(TOKEN_STAR);
+        case ':': return makeToken(TOKEN_COLON);
         case '!':
             return makeToken(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
         case '=':
