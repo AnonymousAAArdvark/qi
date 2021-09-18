39655ab9cc9e5bbbae06450fa40d552be96e5db8
Andrew Yang <andrewhuiyang0830@gmail.com>
2021-09-17 23:47:15 -0700

    implement round native

--- stdlib.c
+++ stdlib.c
@@ -10,6 +10,16 @@
 
 #include "stdlib.h"
 
+static bool nativeError(Value* args, char* msg, ...) {
+    va_list list;
+    char error[100];
+    va_start(list, msg);
+    vsprintf(error, msg, list);
+    args[-1] = OBJ_VAL(copyString(error, 100));
+    va_end(list);
+    return false;
+}
+
 bool clockNative(int argCount, Value* args) {
     args[-1] = NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
     return true;
@@ -40,12 +50,16 @@ bool maxNative(int argCount, Value* args) {
 }
 
 bool roundNative(int argCount, Value* args) {
-    int precision = 0;
     if (argCount < 1 || argCount > 2) {
-        char error[40];
-        sprintf(error, "Expected 1 to 2 arguments but got %d", argCount);
-        args[-1] = OBJ_VAL(copyString(error, 40));
-        return false;
+        return nativeError(args, "Expected 1 to 2 arguments but got %d.", argCount);
     }
+    if (argCount == 2 && !IS_NUMBER(args[1])) {
+        return nativeError(args, "Argument 2 (precision) must be of type 'number'.");
+    }
+    if (!IS_NUMBER(args[0])) {
+        return nativeError(args, "Argument 1 (input) must be of type 'number'.");
+    }
+    double shift =  pow(10.0, AS_NUMBER(args[1]));
+    args[-1] = NUMBER_VAL(round(AS_NUMBER(args[0]) * shift) / shift);
     return true;
 }
