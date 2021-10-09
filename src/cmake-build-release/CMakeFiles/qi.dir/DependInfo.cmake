1362c9ff0de51b20dd7254a95788d9af4de234df
Andrew Yang <andrewhuiyang0830@gmail.com>
2021-10-08 22:14:52 -0700

    added tests & testing suite

---
+++ src/cmake-build-release/CMakeFiles/qi.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/andrewyang/CLionProjects/qi/src/chunk.c" "/Users/andrewyang/CLionProjects/qi/src/cmake-build-release/CMakeFiles/qi.dir/chunk.c.o"
+  "/Users/andrewyang/CLionProjects/qi/src/compiler.c" "/Users/andrewyang/CLionProjects/qi/src/cmake-build-release/CMakeFiles/qi.dir/compiler.c.o"
+  "/Users/andrewyang/CLionProjects/qi/src/debug.c" "/Users/andrewyang/CLionProjects/qi/src/cmake-build-release/CMakeFiles/qi.dir/debug.c.o"
+  "/Users/andrewyang/CLionProjects/qi/src/lib.c" "/Users/andrewyang/CLionProjects/qi/src/cmake-build-release/CMakeFiles/qi.dir/lib.c.o"
+  "/Users/andrewyang/CLionProjects/qi/src/main.c" "/Users/andrewyang/CLionProjects/qi/src/cmake-build-release/CMakeFiles/qi.dir/main.c.o"
+  "/Users/andrewyang/CLionProjects/qi/src/memory.c" "/Users/andrewyang/CLionProjects/qi/src/cmake-build-release/CMakeFiles/qi.dir/memory.c.o"
+  "/Users/andrewyang/CLionProjects/qi/src/object.c" "/Users/andrewyang/CLionProjects/qi/src/cmake-build-release/CMakeFiles/qi.dir/object.c.o"
+  "/Users/andrewyang/CLionProjects/qi/src/scanner.c" "/Users/andrewyang/CLionProjects/qi/src/cmake-build-release/CMakeFiles/qi.dir/scanner.c.o"
+  "/Users/andrewyang/CLionProjects/qi/src/table.c" "/Users/andrewyang/CLionProjects/qi/src/cmake-build-release/CMakeFiles/qi.dir/table.c.o"
+  "/Users/andrewyang/CLionProjects/qi/src/value.c" "/Users/andrewyang/CLionProjects/qi/src/cmake-build-release/CMakeFiles/qi.dir/value.c.o"
+  "/Users/andrewyang/CLionProjects/qi/src/vm.c" "/Users/andrewyang/CLionProjects/qi/src/cmake-build-release/CMakeFiles/qi.dir/vm.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
